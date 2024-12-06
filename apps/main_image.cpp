c2ea9c61b6370eeb4f2b89902cd4bd42be15d387
feldmeyn <feldmeyn@unc.edu>
2024-12-06 12:27:43 -0500

    have a good christmas break

--- apps/main_image.cpp
+++ apps/main_image.cpp
@@ -3,10 +3,18 @@
  */
 
 #include <stdio.h>
+#include <chrono>
+#include <thread>
 
 extern int main_image(int argc, const char* argv[]);
 
 int main(int argc, const char* argv[]) {
+    
+    for(int i=0;i<4;i++){
+        sleep_for(60s);
+    }
+
+
     return main_image(argc, argv);
 }
 
